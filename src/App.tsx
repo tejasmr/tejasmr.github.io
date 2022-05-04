4dd20635ed0de980ce5d57f04174f31734f2e0dd
Tejas M R <totejasmr@gmail.com>
2022-05-04 19:47:38 +0530

    Fixed formatting

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import NavBar from './components/NavBar/NavBar';
 
 export default class App extends Component {
   render(): JSX.Element {
-    return <NavBar />
+    return (
+      <NavBar />
+    );
   }
 }
 
